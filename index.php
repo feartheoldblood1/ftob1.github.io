38a5b1a8f56fcae06946e2908468be9751b27f0f
feartheoldblood1 <67269351+feartheoldblood1@users.noreply.github.com>
2020-06-22 18:14:05 +0300

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>ImagesBayda&Lobachev</title>
+	<title>ImagesBaydaLobachev</title>
 	<link rel="stylesheet" href="css/main.css">
 </head>
 <body>
